8e306b102d8c1068643f22be161934793849bde1
StealthWorm <thierrypitela@hotmail.com>
2023-11-03 22:36:22 -0300

    fix: adjusts to TextInput to includereference to the size variant and other properties

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -3,14 +3,15 @@ import { Input, Prefix, TextInputContainer } from './styles'
 
 export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string
+  containerProps?: ComponentProps<typeof TextInputContainer>
 }
 
 // "ref" é uma maneira do react acessar a referencia do elemnto na DOM (arvore de elementos), permitindo usar metodos como focus direto no input
 // "ElementRef" extrai a tipagem de um outro componente
 export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
-  ({ prefix, ...props }: TextInputProps, ref) => {
+  ({ prefix, containerProps, ...props }: TextInputProps, ref) => {
     return (
-      <TextInputContainer>
+      <TextInputContainer {...containerProps}>
         {/* "!!" é um sinal para tranformar um valor em uma checagem booleana */}
         {!!prefix && <Prefix>{prefix}</Prefix>}
         <Input ref={ref} {...props} />
